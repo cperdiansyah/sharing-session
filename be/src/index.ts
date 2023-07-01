f8cc1a6269766cbe3d7d8913d1355c72190e8c39
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-07-01 11:51:58 +0700

    feat : setup cors

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, NextFunction, Request, Response } from 'express'
 import dotenv from 'dotenv'
-import { NODE_ENV, PORT } from './utils/index.js'
+import cors from 'cors'
+
+import { CORS_LOCAL, NODE_ENV, PORT } from './utils/index.js'
 
 dotenv.config()
 
@@ -11,6 +13,30 @@ const app: Express = express()
 const port: number = PORT ? Number(process.env.PORT) : 3000
 app.use(express.json())
 
+/* Cors Setting */
+const whitelist: string[] = [CORS_LOCAL]
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
+app.use(
+  cors({
+    origin: function (origin: any, callback: CallableFunction) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true,
+  })
+)
 /* Routes */
 app.get('/', (req: Request, res: Response) => {
   res.send({
--- be/src/utils/index.ts
+++ be/src/utils/index.ts
@@ -2,4 +2,10 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const { NODE_ENV, PORT = 5000, MOVIE_API, MOVIE_API_TOKEN } = process.env
+export const {
+  NODE_ENV,
+  PORT = 5000,
+  MOVIE_API,
+  MOVIE_API_TOKEN,
+  CORS_LOCAL = '',
+} = process.env
